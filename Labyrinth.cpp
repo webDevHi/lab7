bb26521dcef345fed205eb57d82a97d4fe8ae138
Marion Office <marionoffice@Marions-MacBook-Pro.local>
2024-10-24 09:26:59 -0500

    created variables to check if items are found, started function to check if move is legal

---
+++ Labyrinth.cpp
@@ -0,0 +1,22 @@
+#include "Labyrinth.h"
+
+bool isPathToFreedom(MazeCell* start, const std::string& moves) {
+    
+    bool foundSpellBook = false;
+    bool foundWand = false;
+    bool foundPotion = false;
+
+    while (!foundSpellBook && !foundWand && !foundPotion){
+        
+    }
+    check if item exist, if it does set to true
+    check if move is legal
+
+    (void) start;
+    (void) moves;
+    return false;
+}
+
+bool isLegal(MazeCell* currCell, const std::string& moves ){
+
+}
